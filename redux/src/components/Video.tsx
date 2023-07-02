82b4a1ca1544df39ce9948d04d2dead8ba483c36
Isaias Martins <isaias.martinsdecarvalho@gmail.com>
2023-07-02 17:54:37 -0300

    finalizacão do aside

--- redux/src/components/Video.tsx
+++ redux/src/components/Video.tsx
@@ -1,13 +1,27 @@
 import Player from "react-player";
+import { useDispatch } from "react-redux";
+import { next, useCurrentLesson } from "../store/slices/player";
+import { useAppSelector } from "../store";
 
 export function Video(){
+
+    const dispatch = useDispatch();
+
+    const { currentLesson } = useCurrentLesson()
+
+    function handlePlayNext(){
+        dispatch(next())
+    };
+
     return(
         <div className="w-full bg-zinc-950 aspect-video">
             <Player
                 width="100%"
                 height="100%"
                 controls
-                url="https://www.youtube.com/watch?v=oPOKpSFqy-I"
+                playing
+                onEnded={handlePlayNext}
+                url={`https://www.youtube.com/watch?v=${currentLesson.id}`}
             />
         </div>
     )
